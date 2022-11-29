bee3f194201dda9a68537f5819e0a2449310c0b8
jay.zaveri <jay.zaveri@augustinfotech.com>
2022-11-29 19:38:43 +0530

    comment db

--- App.py
+++ App.py
@@ -5,28 +5,28 @@ import FCMManager as fcm
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 ma = Marshmallow(app) 
 
 
-class Testdb(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    u_id = db.Column(db.Integer)
-    d_token = db.Column(db.String(1000))
+# class Testdb(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     u_id = db.Column(db.Integer)
+#     d_token = db.Column(db.String(1000))
 
-    def __init__(self, u_id, d_token):
-        self.u_id = u_id
-        self.d_token = d_token
+#     def __init__(self, u_id, d_token):
+#         self.u_id = u_id
+#         self.d_token = d_token
 
 
-class PostSchema(ma.Schema):
-    class Meta:
-        fields = ("u_id", "d_token")
+# class PostSchema(ma.Schema):
+#     class Meta:
+#         fields = ("u_id", "d_token")
 
-post_schema = PostSchema()
-posts_schema = PostSchema(many=True)
+# post_schema = PostSchema()
+# posts_schema = PostSchema(many=True)
 
 
 @app.route('/post', methods=['POST'])
@@ -39,10 +39,11 @@ def add_post():
     fcm.sendPush("Hi", "This is my next msg", [d_token])
     print("d_token=", d_token)
     # d_token = "test data"
-    my_posts = Testdb(u_id, d_token)
-    db.session.add(my_posts)
-    db.session.commit()
-    return post_schema.jsonify(my_posts)
+    # my_posts = Testdb(u_id, d_token)
+    # db.session.add(my_posts)
+    # db.session.commit()
+    # return post_schema.jsonify(my_posts)
+    return 1
 
 
 if __name__ == "__main__":

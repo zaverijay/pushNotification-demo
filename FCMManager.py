2ceb65e2a5e13d756e1d8f42f2d3a5bd97698e92
jay.zaveri <jay.zaveri@augustinfotech.com>
2022-11-29 17:16:14 +0530

    new commit

--- FCMManager.py
+++ FCMManager.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate(r"C:\Users\Jay Zaveri\Desktop\FCM testing\serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 def sendPush(title, msg, registration_token, dataObject=None):
